5d612d12f922d7f14fda27fed90b33bfdacccaa2
Vitaliy Bizilia <biziliavv@gmail.com>
2015-06-16 15:20:24 +0300

    ggg

--- src/test/java/com/saucelabs/appium/ShiwaforceTest.java
+++ src/test/java/com/saucelabs/appium/ShiwaforceTest.java
@@ -53,7 +53,7 @@ public class ShiwaforceTest {
         //       capabilities.setCapability("app", "/Users/admin/Library/Developer/Xcode/DerivedData/karmaplatform-dlgwpadqmehxlpdcconqkavvsbun/Build/Products/Debug-iphoneos/karmaplatform.app");
 
 //        driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
-        driver = new AppiumDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities) {
+        driver = new AppiumDriver(new URL("http://127.0.0.1:1111/wd/hub"), capabilities) {
 
 
             public MobileElement scrollTo(String s) {
